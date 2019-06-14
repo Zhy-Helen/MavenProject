827d4fbb0c41d3e087e4d57b6956722c9d25f11b
01476531 <zhangyan2@haier.com>
2019-06-14 17:52:28 +0800

    Maven Java项目第一次提交

---
+++ src/test/java/com/haier/ai/all_test.java
@@ -0,0 +1,15 @@
+package com.haier.ai;
+
+import java.lang.String;
+
+import static jdk.nashorn.internal.objects.Global.print;
+
+public class all_test {
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("hello world!");
+    }
+
+}
